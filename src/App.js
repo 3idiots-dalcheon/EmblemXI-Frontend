4bcbbfcc01aff3503e810b67b8e13061ffdf4904
syk001108 <1108suyeon@gmail.com>
2025-03-26 15:09:50 +0900

    feat: 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./pages/Main/Main";
+import { Play } from "./pages/Play/Play";
+import { Result } from "./pages/Result/Result";
+import Layout from "./components/Layout"; // Layout 가져오기
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout> {/* Layout을 감싸서 모든 페이지에 배경 적용 */}
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/Play" element={<Play />} />
+            <Route path="/Result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
